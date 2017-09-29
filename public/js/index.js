0af84836c7e0fe46b1d21c4e21157b8512943c39
Matthew Pliss <matthew@mg2it.com>
2017-09-29 14:14:32 -0700

    Add newMessage and createMessage events. Tight.

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+
+
+
+//=============================================================================
+// INDEX.HTML SOCKET Calls
+//=============================================================================
+var socket = io();    // This initiates a call to the server from the client to keep a connection open
+
+socket.on('connect', function() {     // Will fire off anytime we crete a connection to the server
+   console.log('Connected to Server.');   // This logs to the client console
+
+//    socket.emit('createEmail', {     // We put the emitter inside the connect so it doesn't fire beforehand
+//       to: 'right@ous.com',
+//       text: 'Name the time and place, and wear something sexy.'
+//    });
+
+   socket.emit('createMessage', {
+      from: 'Jesse',
+      text: 'You know it!'
+   });
+});
+
+socket.on('disconnect', function() {  // Fires off anytime the server goes down
+   console.log('Dropped yo connection, dawg.');
+});
+
+// socket.on('newEmail', function(email) {
+//    console.log("You've got Mail!", email);
+// });
+
+socket.on('newMessage', function(message) {
+   console.log(message);
+});
--- server/server.js
+++ server/server.js
@@ -25,10 +25,38 @@ const port = process.env.PORT || 3000;
 
 
 // This lets us listen for a new connection (like a new user connecting)
-io.on('connection', (socket) => {
-   console.log('New User connected.');
+// This intial connection event will hold other io events in it until disconnect destroys it.
+io.on('connection', (socket) => {      // Fires off anytime a web browser connects to io()
+   console.log('New User connected.');    // Logs to server console
 
-   socket.on('disconnect', () => {
+   // // Emit can have two arguments (emitter, data)
+   // // Emitter name needs to match name of client (vice-versa)
+   // // Data is not required, but can be anything (usually an object)
+   // socket.emit('newEmail', {
+   //    from: 'right@ous.com',
+   //    text: 'Pop my cherry',
+   //    createdAt: 12345
+   // });
+   //
+   // socket.on('createEmail', (newEmail) => {
+   //    console.log('Create Email: ', newEmail);
+   // });
+
+   socket.emit('newMessage', {
+      from: 'Leroy',
+      text: 'Jesse is Gay',
+      createdAt: 1700
+   });
+
+   socket.on('createMessage', (message) => {
+      console.log('New Message:', message);
+   });
+
+//who
+//text
+//createdAt       console.log(it);
+
+   socket.on('disconnect', () => {     // Fires off anytime a web browser(client) leaves the server (closes)
       console.log('Homie left, yo.');
    });
 });
